17a51cdfb0380431feffce010eb9b7fe932b58f1
Tiago Ribeiro <tiago.r.ribeiro@gmail.com>
2023-10-30 01:58:11 +0000

    utils_smote_update

--- src/utils.py
+++ src/utils.py
@@ -6,14 +6,16 @@
 
 import os
 import logging
+
 import pandas as pd
+import numpy as np
+from tabulate import tabulate
 from sklearn.preprocessing import (
     StandardScaler,
     MinMaxScaler,
     RobustScaler,
     LabelEncoder,
 )
-import numpy as np
 
 
 def save_results_to_csv(results, csv_file_path):
@@ -59,7 +61,10 @@ def load_dataset(data_directory, augmentation="None", ignore_columns=None):
     # Define file paths based on augmentation option
     file_paths = {
         "None": {"train": "EdgeIIot_train_100k.csv", "test": "EdgeIIot_test.csv"},
-        "SMOTE": {"train": "train_smote.csv", "test": "encoded_testData.csv"},
+        "SMOTE": {
+            "train": "EdgeIIot_train_smote_v2.csv",
+            "test": "EdgeIIot_test_enc_v2.csv",
+        },
         "SMOTE-NC": {"train": "train_smotenc.csv", "test": "EdgeIIot_test.csv"},
         "RealTabFormer": {
             "train": "EdgeIIot_train_100k_RealTabFormer.csv",
@@ -104,14 +109,13 @@ def load_dataset(data_directory, augmentation="None", ignore_columns=None):
     return df_train, df_test
 
 
-def one_hot_encode_categorical(X_train, X_test, random_state=None):
+def encode_categorical(X_train, X_test):
     """
     One-hot encode categorical features in X_train and X_test.
 
     Parameters:
     - X_train: pd.DataFrame, training dataset.
     - X_test: pd.DataFrame, test dataset.
-    - random_state: int or None, random state for train_test_split.
 
     Returns:
     - X_train_enc: pd.DataFrame, one-hot encoded training dataset.
@@ -119,26 +123,35 @@ def one_hot_encode_categorical(X_train, X_test, random_state=None):
     """
 
     # Extract categorical features
-    cat_features_train = X_train.select_dtypes(include="object").columns
-    cat_features_test = X_test.select_dtypes(include="object").columns
+    cat_features_train = X_train.select_dtypes(include=["object", "category"]).columns
+    cat_features_test = X_test.select_dtypes(include=["object", "category"]).columns
+
+    X_comb = pd.concat([X_train, X_test], axis=0)
 
     # Check if there are categorical features
     if cat_features_train.empty and cat_features_test.empty:
         print("No categorical features found. Returning original datasets.")
         return X_train, X_test
 
-    # Concatenate X_train and X_test
-    X_comb = pd.concat([X_train[cat_features_train], X_test[cat_features_test]], axis=0)
+    cat_features = list(set(cat_features_train) | set(cat_features_test))
 
-    # Apply one-hot encoding (get_dummies)
-    X_comb_enc = pd.get_dummies(X_comb, dtype="int8")
+    print(f"Categorical features to be encoded:\n")
+    print("\n".join(cat_features))
+
+    # Apply one-hot encoding (get_dummies) only to categorical features
+    X_comb_enc = pd.get_dummies(
+        X_comb, columns=cat_features_train, drop_first=True, dtype="int8"
+    )
+
+    # Drop original categorical columns
+    X_comb_enc = X_comb_enc.drop(columns=cat_features, errors="ignore")
 
     # Split back into X_train and X_test
     rows_train = len(X_train)
     X_train_enc = X_comb_enc.iloc[:rows_train, :]
     X_test_enc = X_comb_enc.iloc[rows_train:, :]
 
-    print("Encoding complete.")
+    print("\nEncoding complete.")
     print(
         f"No of features before encoding: {X_train.shape[1]}"
         + "\n"
@@ -146,6 +159,10 @@ def one_hot_encode_categorical(X_train, X_test, random_state=None):
     )
 
     return X_train_enc, X_test_enc
+
+
+# Por favor corrige e completa esta fução de modo a que ela facoa get dumies so
+# às varivaies categoricas e as junte às restantes, eliminado das categoricas e objetco originais
 
 
 def encode_labels(y_train, y_test):
@@ -180,61 +197,18 @@ def encode_labels(y_train, y_test):
     return y_train_enc, y_test_enc, le
 
 
-# def scale_data(X_train, X_test, scaler_type="standard"):
-#     """
-#     Scale the input data using the specified scaler.
-
-#     Parameters:
-#     X_train (np.array): The training data to be scaled.
-#     X_test (np.array): The test data to be scaled.
-#     scaler_type (str): The type of scaler to use. Options are 'standard', 'minmax', and 'robust'. Default is 'standard'.
-
-#     Returns:
-#     X_train_scaled (np.array): The scaled training data.
-#     X_test_scaled (np.array): The scaled test data.
-
-#     Raises:
-#     ValueError: If the scaler_type is not 'standard', 'minmax', or 'robust'.
-#     Exception: If there was an error during scaling.
-#     """
-
-#     if not isinstance(X_train, np.ndarray) or not isinstance(X_test, np.ndarray):
-#         raise ValueError("Input data should be numpy array")
-#     if scaler_type not in ["standard", "minmax", "robust"]:
-#         raise ValueError(f"Unknown scaler: {scaler_type}")
-
-#     if scaler_type == "standard":
-#         scaler = StandardScaler()
-#     elif scaler_type == "minmax":
-#         scaler = MinMaxScaler()
-#     elif scaler_type == "robust":
-#         scaler = RobustScaler()
-
-#     try:
-#         X_train_scaled = scaler.fit_transform(X_train)
-#         X_test_scaled = scaler.transform(X_test)
-#         logging.info(f"Scaling successful with {scaler_type} scaler.")
-#     except Exception as e:
-#         logging.error(f"Error during scaling: {str(e)}")
-#         raise
-
-#     pretty_print_stats(X_train_scaled, X_test_scaled)
-
-#     return X_train_scaled, X_test_scaled
-
-
 def scale_data(X_train, X_test, scaler_type="standard"):
     """
     Scale the input data using the specified scaler.
 
     Parameters:
-    X_train (np.array): The training data to be scaled.
-    X_test (np.array): The test data to be scaled.
-    scaler_type (str): The type of scaler to use. Options are 'standard', 'minmax', and 'robust'. Default is 'standard'.
+    - X_train (np.array): The training data to be scaled.
+    - X_test (np.array): The test data to be scaled.
+    - scaler_type (str): The type of scaler to use. Options are 'standard', 'minmax', and 'robust'. Default is 'standard'.
 
     Returns:
-    X_train_scaled (np.array): The scaled training data.
-    X_test_scaled (np.array): The scaled test data.
+    - X_train_scaled (np.array): The scaled training data.
+    - X_test_scaled (np.array): The scaled test data.
 
     Raises:
     ValueError: If the scaler_type is not 'standard', 'minmax', or 'robust'.
@@ -272,8 +246,8 @@ def pretty_print_stats(X_train, X_test):
     Pretty print the mean and standard deviation of the input data.
 
     Parameters:
-    X_train (np.array): The training data.
-    X_test (np.array): The test data.
+    - X_train (np.array): The training data.
+    - X_test (np.array): The test data.
     """
 
     # Calculate mean and standard deviation
@@ -286,94 +260,36 @@ def pretty_print_stats(X_train, X_test):
     print(f"{'Test:':<10}{test_mean:<10.3f}{test_std:<10.3f}")
 
 
-# def save_results_to_csv(results, file_path, columns=None, append=True):
-#     """
-#     Saves the results of experiments to a CSV file. Creates the file if it
-#     doesn't exist. If columns are provided, uses them as column names;
-#     otherwise, uses keys from the results dictionary.
-
-#     Parameters:
-#     - results (dict or list): A dictionary or list containing the results to be saved.
-#     - file_path (str): The path of the CSV file.
-#     - columns (list, optional): A list of column names. If None, uses keys from the results dictionary.
-#     - append (bool, optional): If True, appends the results to the existing file. If False, overwrites the file.
-
-#     Returns:
-#     - None
-#     """
-
-#     # If results is a list, convert it to a dictionary with default column names
-#     if isinstance(results, list):
-#         results = {'Column{}'.format(
-#             i + 1): results[i] for i in range(len(results))}
-
-#     # If columns are not provided, use keys from the results dictionary
-#     if columns is None:
-#         columns = list(results.keys())
-
-#     # Check if the file already exists
-#     file_exists = os.path.exists(file_path)
-
-#     # Append or create a new CSV file
-#     if append and file_exists:
-#         existing_data = pd.read_csv(file_path)
-#         new_data = pd.DataFrame(results, columns=columns, index=[0])
-#         combined_data = pd.concat([existing_data, new_data], ignore_index=True)
-#         combined_data.to_csv(file_path, index=False)
-#         print(f"Results appended to the existing file: {file_path}")
-#     else:
-#         new_data = pd.DataFrame(results, columns=columns, index=[0])
-#         new_data.to_csv(file_path, index=False)
-#         if file_exists and not append:
-#             print(f"Existing file overwritten with new results: {file_path}")
-#         elif not file_exists:
-#             print(f"New CSV file created: {file_path}")
+def format_value(value):
+    return "{:.2f}%".format(value * 100)
 
 
-# def save_results_to_csv(results, file_path, columns=None, append=True):
-#     """
-#     Saves the results of experiments to a CSV file. Creates the file if it
-#     doesn't exist. If columns are provided, uses them as column names;
-#     otherwise, uses keys from the results dictionary.
+def print_results_table(results):
+    """
+    Print the results with tabulate.
 
-#     Parameters:
-#     - results (dict or list): A dictionary or list containing the results to be saved.
-#     - file_path (str): The path of the CSV file.
-#     - columns (list, optional): A list of column names. If None, uses keys from the results dictionary.
-#     - append (bool, optional): If True, appends the results to the existing file. If False, overwrites the file.
+    Parameters:
+    - results (dict): The results dictionary.
+    """
 
-#     Returns:
-#     - None
-#     """
+    formatted_results = {
+        key: format_value(value)
+        for key, value in results.items()
+        if key not in ["model", "augmentations", "timestamp"]
+    }
 
-#     # Create the directory if it doesn't exist
-#     directory = os.path.dirname(file_path)
-#     if not os.path.exists(directory):
-#         os.makedirs(directory)
-
-#     # If results is a list, convert it to a dictionary with default column names
-#     if isinstance(results, list):
-#         results = {'Column{}'.format(
-#             i + 1): results[i] for i in range(len(results))}
-
-#     # If columns are not provided, use keys from the results dictionary
-#     if columns is None:
-#         columns = list(results.keys())
-
-#     # Check if the file already exists
-#     file_exists = os.path.exists(file_path)
-
-#     # Append or create a new CSV file
-#     if append and file_exists:
-#         existing_data = pd.read_csv(file_path)
-#         new_data = pd.DataFrame(results, columns=columns, index=[0])
-#         combined_data = pd.concat([existing_data, new_data], ignore_index=True)
-#         combined_data.to_csv(file_path, index=False)
-#         print(f"Results appended to the existing file: {file_path}")
-#     else:
-#         new_data = pd.DataFrame(results, columns=columns, index=[0])
-#         new_data.to_csv(file_path, index=False)
-#         if file_exists and not append:
-#             print(f"Existing file overwritten with new results: {file_path}")
-#         elif not file_exists:
-#             print(f"New CSV file created: {file_path}")
+    print(
+        tabulate(
+            [
+                ["Accuracy", formatted_results["accuracy"]],
+                ["Precision (macro)", formatted_results["precision_macro"]],
+                ["Recall (macro)", formatted_results["recall_macro"]],
+                ["F1 (macro)", formatted_results["f1_macro"]],
+                ["Precision (weighted)", formatted_results["precision_weighted"]],
+                ["Recall (weighted)", formatted_results["recall_weighted"]],
+                ["F1 (weighted)", formatted_results["f1_weighted"]],
+            ],
+            headers=["Metric", "Value"],
+            tablefmt="fancy_grid",
+        )
+    )
